318b1766574d3c604e99ee75865aa95dbe620e50
snowmantw <snowmantw@gmail.com>
2013-03-16 22:14:52 +0800

    RESTful methods can works. Understood the decoration things in Python.

--- source/apis.py
+++ source/apis.py
@@ -16,6 +16,9 @@ render_txt = lambda message: message
 render_html = lambda message: '<html><body>%s</body></html>'%message
 
 class Lxc(webapp2.RequestHandler):
+
+    # Need to add decoration upon every method.
+    
     @mimerender(
         default = 'html'
     ,   html = render_html
@@ -28,6 +31,45 @@ class Lxc(webapp2.RequestHandler):
             return {'message': 'GET /lxc/'}
         else:
             return {'message': 'GET /lxc/%s/'%_id}
+
+    @mimerender(
+        default = 'html'
+    ,   html = render_html
+    ,   json = render_json
+    ,   xml = render_xml
+    ,   txt = render_txt
+    )
+    def put(self, _id):
+        if not _id:
+            return {'message': 'PUT /lxc/'}
+        else:
+            return {'message': 'PUT /lxc/%s/'%_id}
+
+    @mimerender(
+        default = 'html'
+    ,   html = render_html
+    ,   json = render_json
+    ,   xml = render_xml
+    ,   txt = render_txt
+    )
+    def post(self, _id):
+        if not _id:
+            return {'message': 'POST/lxc/'}
+        else:
+            return {'message': 'POST /lxc/%s/'%_id}
+
+    @mimerender(
+        default = 'html'
+    ,   html = render_html
+    ,   json = render_json
+    ,   xml = render_xml
+    ,   txt = render_txt
+    )
+    def delete(self, _id):
+        if not _id:
+            return {'message': 'DELETE /lxc/'}
+        else:
+            return {'message': 'DELETE /lxc/%s/'%_id}
 
 app = webapp2.WSGIApplication([
     ('/lxc/(.*)', Lxc)
